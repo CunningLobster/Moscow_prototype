003ce5a99e00fb47f8d8a040e523b128b2a506b2
Yan Cherepansky <cherepansky.yan@yandex.ru>
2023-05-21 13:43:46 +0600

    изменения PlayerMover.cs, создание префабов

--- Assets/Code/Scripts/Movement/PlayerMover.cs
+++ Assets/Code/Scripts/Movement/PlayerMover.cs
@@ -7,24 +7,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMover : MonoBehaviour
 {
-    [SerializeField] private GameObject plain;
-    [SerializeField] private GameObject player;
-    [SerializeField] private float velocity = 10;
+    [SerializeField] private float speed = 10;
+    private float speedMultiplier = 1000;
+
     private Rigidbody playerRigidBody;
     private Vector2 playerDirection;
 
-    // Start is called before the first frame update
     void Start()
     {
-        playerRigidBody = player.GetComponent<Rigidbody>();
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 vertical = player.transform.forward * playerDirection.y;
-        Vector3 horizontal = player.transform.right * playerDirection.x;
-        playerRigidBody.velocity = (vertical + horizontal) * velocity;
+        Vector3 vertical = transform.forward * playerDirection.y;
+        Vector3 horizontal = transform.right * playerDirection.x;
+        playerRigidBody.velocity = (vertical + horizontal) * speed * speedMultiplier * Time.fixedDeltaTime;
     }
 
     public void OnMove(InputAction.CallbackContext context)
