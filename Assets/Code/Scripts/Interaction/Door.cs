05affa24f81e2f86129fd6d8b37b684be4e35b55
Yan Cherepansky <cherepansky.yan@yandex.ru>
2023-05-24 23:57:01 +0600

    переход между сценами

--- Assets/Code/Scripts/Interaction/Door.cs
+++ Assets/Code/Scripts/Interaction/Door.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Code.Scripts.Systems;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Door : InteractableObject
 {
-
+    [SerializeField] string _sceneName;
 
     public override async void Interact(InputAction.CallbackContext context)
     {
@@ -18,6 +19,13 @@ public class Door : InteractableObject
                 await ComeToInteractionPoint();
 
                 Debug.Log("Open door");
+
+                if (string.IsNullOrEmpty(_sceneName))
+                {
+                    Debug.LogError("Scene name is empty");
+                    return;
+                }
+                FindObjectOfType<LevelManager>().LoadScene(_sceneName);
 
             }
         }
