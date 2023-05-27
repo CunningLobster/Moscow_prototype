6e7847259dcf11b6f49bebf49c909af217ef9a6d
Ibi-san <ibragim_nasirov@mail.ru>
2023-05-27 03:14:14 +0300

    Animations
    Коррекция спрайтов персонажа, добавлены анимации взаимодействия и размышления.

--- Assets/Code/Scripts/Systems/MonologueComponent.cs
+++ Assets/Code/Scripts/Systems/MonologueComponent.cs
@@ -6,7 +6,15 @@ namespace Code.Scripts.Systems
     {
         [TextArea] public string MonologueText; //Текст, который будет появляться в окне мыслей ГГ 
 
+        [SerializeField] private Animator playerAnimator;
+
+        private static readonly int Think = Animator.StringToHash("Think");
+
         //Вызывать этот метод, чтобы показать мысли персонажа
-        public void Interact() => MonologueManager.Instance.ShowMonologue(this);
+        public void Interact()
+        {
+            MonologueManager.Instance.ShowMonologue(this);
+            if (playerAnimator != null) playerAnimator.SetTrigger(Think);
+        }
     }
 }
