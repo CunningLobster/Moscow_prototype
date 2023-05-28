a05fc56fb3fc45c007e5a1cf1b4a558e9a5b61b2
trance963 <94801903+trance963@users.noreply.github.com>
2023-05-28 15:07:20 +0400

    Изменен саундменеджер

--- Assets/Code/Scripts/Systems/SoundManager.cs
+++ Assets/Code/Scripts/Systems/SoundManager.cs
@@ -5,7 +5,7 @@ public class SoundManager: MonoBehaviour
 {
     [SerializeField] AudioSource MusicAudioSource; //пока выжимаем все из одного аудио ресурса
     [SerializeField] AudioSource SoundAudioSource;
-    [SerializeField] AudioClip[] audioClipsSound; //наш набор звуков 
+    [SerializeField] AudioClip[] audioClipsSound; //наш набор звуков
     [SerializeField] AudioClip[] audioClipsMusic; //здесь набор музыки
 
     //**************************************************МУЗЫКА*************************************************
@@ -17,44 +17,15 @@ public class SoundManager: MonoBehaviour
     //**************************************************МУЗЫКА*************************************************
     //---------------------------------------------------------------------------------------------------------
     //**************************************************ЗВУКИ**************************************************
-    public void SituationSound(NameOfSound situation)//Ситуационный звук проигрываем через этого метод. Что бы не путаться, пишем в него тот звук, который хотим.
-    {
-        switch (situation) //вызываем из свича по имени нужный звук. Названия совпадают с названиями в ассетах
-        {
-            case NameOfSound.BrokenGlass://звук разбитого стекла
-                CaseServiceSituation(0);// тут уже используем индекс, без этих ваших рандомов. Знаем, чего хотим. 
-                break;
-            case NameOfSound.FoleyDoorLock://открываем дверь
-                CaseServiceSituation(1);// индекс соотвествует очередности в инспекторе
-                break;
-            case NameOfSound.WalkOneStep://звук одного шага
-                CaseServiceSituation(2);
-                break;
-            case NameOfSound.CheckGarbage://шумим проверяя мусорку
-                CaseServiceSituation(3);
-                break;
-            case NameOfSound.DogBarking://собака лает за дверью соседа
-                CaseServiceSituation(4);
-                break;
-            case NameOfSound.DoorBell://обыкновенный звонок в дверь
-                CaseServiceSituation(5);
-                break;
-            case NameOfSound.ReadingBook://звук чтения книги или записки
-                CaseServiceSituation(6);
-                break;
-            case NameOfSound.ThunderWeather://раскаты грома, звуки грозовой погоды без шума дождя
-                CaseServiceSituation(7);
-                break;
-            default:
-                break;
-        }
 
-        void CaseServiceSituation(int SituationNumberOfSound)// сервисный метод для удобоваримости вида свича
-        {
-            SoundAudioSource.PlayOneShot(audioClipsSound[SituationNumberOfSound]);// проигрываем из аудиосорса элемент с индексом
-        }
+    AudioClip LoadClip(NameOfSound name) //метод загрузки ситуационного звука
+    {
+        string path = "Audio/Sounds/" + name; //путь плюс имя
+        AudioClip clip = Resources.Load<AudioClip>(path); //загрузка
+        return clip;
     }
 
+    
 
     public enum NameOfSound
     {
