c5c5bfd8b99cf32a7de6be5406786690b84fea16
Ibi-san <ibragim_nasirov@mail.ru>
2023-05-28 22:16:05 +0300

    Neighborhood
    Добавлен текст в сцену

--- Assets/Code/Scripts/Interaction/GarbageBin.cs
+++ Assets/Code/Scripts/Interaction/GarbageBin.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Code.Scripts.Systems;
 using UnityEngine;
 
 public class GarbageBin : InteractableObject
 {
     [SerializeField] private Sprite _activatedSprite;
     [SerializeField] private Texture2D _activatedCursor;
+
+    [SerializeField] private MonologueComponent binMonologueObserv;
+    [SerializeField] private MonologueComponent binMonologueInteraction;
 
     protected override IEnumerator RunInteractionRoutine()
     {
@@ -15,6 +19,7 @@ public class GarbageBin : InteractableObject
         yield return ComeToInteractionPoint();
 
         Debug.Log("Came to garbage bag");
+        
 
         while (_isAvailable)
         {
@@ -23,12 +28,14 @@ public class GarbageBin : InteractableObject
                 _isObserved = true;
                 _pointCursor = _activatedCursor;
                 UpdateCursor();
+                MonologueManager.Instance.ShowMonologue(binMonologueObserv);
                 yield break;
             }
             else
             {
                 _isActivated = true;
                 GetComponent<SpriteRenderer>().sprite = _activatedSprite;
+                MonologueManager.Instance.ShowMonologue(binMonologueInteraction);
                 AudioClip clip = FindObjectOfType<SoundManager>().LoadClip(SoundManager.NameOfSound.PlasticBag);
                 FindObjectOfType<AudioSource>().PlayOneShot(clip);
                 //FindObjectOfType<SoundManager>().OldSituationSound(SoundManager.NameOfSound.PlasticBag);
--- Assets/Code/Scripts/Interaction/GarbageContainers.cs
+++ Assets/Code/Scripts/Interaction/GarbageContainers.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Code.Scripts.Systems;
 using UnityEngine;
 
 public class GarbageContainers : InteractableObject
 {
+    [SerializeField] private MonologueComponent containerMonologue;
+    
     protected override IEnumerator RunInteractionRoutine()
     {
         while (_isAvailable)
         {
             _player.GetComponent<PlayerMover>().Target = gameObject;
             yield return ComeToInteractionPoint();
-
+            
+            MonologueManager.Instance.ShowMonologue(containerMonologue);
             FindObjectOfType<SoundManager>().OldSituationSound(SoundManager.NameOfSound.CheckGarbage);
 
             _isAvailable = false;
